6ba0a04f163430ca69c7e55f3dcbbe8df7de319a
drapiigi <82909800+drapiigi@users.noreply.github.com>
2022-05-06 03:03:01 +0000

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
